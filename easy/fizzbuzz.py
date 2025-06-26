231b4154b7ff6ed0f531597337ccd19afe28c40e
reni-renisha <renishagillus17@gmail.com>
2025-06-26 18:24:57 +0530

    Update fizzbuzz.py

--- easy/fizzbuzz.py
+++ easy/fizzbuzz.py
@@ -1,12 +1,12 @@
-# Fizz Buzz 🐝
+
 #version 1
 
-for num in range(1, 101):
-  if num % 3 == 0 and num % 5 == 0:
-    print('FizzBuzz')
-  elif num % 3 == 0:
-    print('Fizz')
-  elif num % 5 == 0:
-    print('Buzz')
+for i in range(1,101):
+  if  i%3==0 and i%5==0:
+    print('fizzbuzz')
+  elif i%3==0:
+    print("Fizz")
+  elif i%5==0:
+    print("Buzz")
   else:
-    print(num)
+    print(i)
