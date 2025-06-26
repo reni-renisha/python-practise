9d80290458128114500288639586a0e6f8e94927
reni-renisha <renishagillus17@gmail.com>
2025-06-26 18:28:33 +0530

    Update fizzbuzz.py

--- easy/fizzbuzz.py
+++ easy/fizzbuzz.py
@@ -1,5 +1,9 @@
-
 #version 1
+# Write a program that prints the numbers from 1 to 100.
+#
+# But for multiples of 3, print "Fizz" instead of the number.
+# For multiples of 5, print "Buzz" instead of the number.
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 for i in range(1,101):
   if  i%3==0 and i%5==0:
