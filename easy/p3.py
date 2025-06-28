49e78be1063db25448c1bd166b5fd93261cf64b0
reni-renisha <renishagillus17@gmail.com>
2025-06-28 20:48:27 +0530

    Create p3.py

---
+++ easy/p3.py
@@ -0,0 +1,13 @@
+n =int(input()) #number of people
+c =int(input()) #tank capacity
+A =[int(input()) for _ in range(n)] #actions
+tank=0
+min_req=0 #Minimum tank level
+for action in A:
+    tank += action  # Add 1 if sell, subtract 1 if buy
+    if tank < min_req:
+        min_req = tank # Store the lowest tank level
+X = -min_req  # Initial oil needed 
+print(X)
+
+
